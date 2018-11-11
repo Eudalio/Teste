d6749560f37b0a2b1415bebead48a1c48bd91771
eudalio <tonnydallios@gmail.com>
2018-11-10 23:36:42 -0300

    Criado Login

---
+++ src/components/Tweet.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import styles from './styles';
+
+export default class Tweet extends Component {
+  render() {
+    return <View />;
+  }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      borderBottomWidth: 1,
+      borderColor: "#eee"
+    },
+  
+    author: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "#1C2022"
+    },
+  
+    content: {
+      fontSize: 15,
+      lineHeight: 20,
+      color: "#1C2022",
+      marginVertical: 10
+    },
+  
+    likeButton: {
+      flexDirection: "row",
+      alignItems: "center"
+    },
+  
+    likeText: {
+      color: "#999",
+      marginLeft: 5
+    }
+  });
+  
